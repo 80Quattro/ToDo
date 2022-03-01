64669513dbeb15f3109842258aa37c8ab1fe0157
80Quattro <muzyczek2@gmail.com>
2022-03-01 13:00:10 +0100

    Select new room and join room option in home screen

--- Views/home.php
+++ Views/home.php
@@ -1,12 +1,12 @@
 <div class="container">
     <div class="row">
-        <div class="col">
+        <div class="col choice selected" id="createRoom">
             <h2>Podaj nazwę użytkownika</h2>
             <form action="/create" method="POST" id="nameForm">
                 <input type="text" name="name" id="nameInput" class="form-control">
             </form>
         </div>
-        <div class="col">
+        <div class="col choice" id="joinRoom">
             <h2>Podaj nazwę pokoju</h2>
             <input type="text" name="roomId" id="roomIdInput" class="form-control">
         </div>
